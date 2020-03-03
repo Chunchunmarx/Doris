a4796ea3a5422a0d192d95113d7255d86b497268
Chunchunmarx <kitty_starstruck@yahoo.com>
2020-03-03 12:21:08 +0200

    cod curatat

--- TextEditorStatic/routes.py
+++ TextEditorStatic/routes.py
@@ -7,42 +7,11 @@ from TextEditorStatic.models import Results
 from TextEditorStatic.lib.flaskcode.views import resource_data
 import ntpath
 
-nume_fisier = ""
-
-
 @app.route('/', methods=['GET','POST'])
 
 def index():
 
-	
-	form = TextForm()
-	if form.validate_on_submit():
-		text_write = open(index.nume_fisier, 'w')
-		text_write.write(form.text_box.data)
-		text_write.close()
-	if request.args.get('messages') is not None:
-		index.nume_fisier = request.args.get('messages')
-		
-		text_read = open(index.nume_fisier, 'r')
-		path = text_read.read()
-		text_read.close()
-		form.text_box.data = path
-		app.config['FLASKCODE_RESOURCE_BASEPATH'] = path
-	print(Results.query.all())
-	return redirect('/flaskcode')    #render_template('index.html', form=form, dtree=dtree)
-
-@app.route('/success', methods=['GET','POST'])
-def upload():
-	if request.method == 'POST':
-		f = request.files['config_file']
-		#print(f.read())
-	
-		
-		path="C:\\Users\\AOprescu\\Desktop\\Texas Tool\\TestFolder"
-		app.config['FLASKCODE_RESOURCE_BASEPATH'] = path
-
-	return redirect('/flaskcode')
-
+	return redirect('/flaskcode')   
 
 @app.route('/runScript',methods=['GET','POST'] )
 def runScript():
@@ -56,8 +25,6 @@ def runScript():
     returned_value = os.system(cmd) 
     db.session.add(Results(test_result=returned_value,test_name=file_name))
     db.session.commit() 
-    print(db.session.query(Results).all())
-    print(returned_value)
     return redirect('flaskcode')
 
 @app.route('/generateUML',methods=['GET','POST'] )
